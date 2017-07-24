410151d2ded15917e9f16a8e738a91385a5d9119
Sownthiryamuthusamy96 <sownthiryamuthusamy@gmail.com>
2017-07-24 16:03:50 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+public static void main(Sting[] args)
+{
+BufferedReader br=newBufferedReader(new InputStreamReader(System.in));
+int n=Integer.parseInt(br.readLine());
+int fact=1;
+for(i=1;i<=n;i++)
+{
+fact=n*i;
+}
+System.out.println(fact);
+}
+}
