20ae6c5d1748fd7cf9a84454980b11b6957121bd
Sownthiryamuthusamy96 <sownthiryamuthusamy@gmail.com>
2017-07-24 15:38:53 +0530

    Create natural1.java

---
+++ natural1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+public static void main(String[] args)
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+int n=Integer.parseInt(br.readLine());
+int i,j,sum=0;
+for(i=0;i<n;i++)
+{
+if(i>=0)
+{
+sum=sum+i;
+j++;
+}
+System.out.println(i);
+}
+}
+}
+
