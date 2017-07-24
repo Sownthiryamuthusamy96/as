ecef6141df089b3f160e197ccd3b45d187afda55
Sownthiryamuthusamy96 <sownthiryamuthusamy@gmail.com>
2017-07-24 12:49:08 +0530

    Create print.java

---
+++ print.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+public static void main(String[] args)
+{
+BufferedReader br=new BuffredReader(new InputStreamReader(System.in));
+String str=br.readLine();
+int i;
+for(i=0;i<5;i++)
+{
+System.out.println("printing word"+str);
+}
+}
+}
